1a4d8e7673295b471d64bb067bd787226aca6f72
Stephanie Strong <sstrong@wideopenwest.com>
2018-02-01 13:40:48 -0700

    Placing mock content in Digital Page

---
+++ scripts/digital.js
@@ -0,0 +1,30 @@
+var animateReviews = function() {
+
+                 var reviews = document.getElementsByClassName('review');
+
+                 var revealFirstReview = function() {
+                     reviews[0].style.opacity = 1;
+                     reviews[0].style.transform = "scaleX(1) translateY(0)";
+                     reviews[0].style.msTransform = "scaleX(1) translateY(0)";
+                     reviews[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+                 };
+
+                 var revealSecondReview = function() {
+                     reviews[1].style.opacity = 1;
+                     reviews[1].style.transform = "scaleX(1) translateY(0)";
+                     reviews[1].style.msTransform = "scaleX(1) translateY(0)";
+                     reviews[1].style.WebkitTransform = "scaleX(1) translateY(0)";
+                 };
+
+                 // var revealThirdReview = function() {
+                 //     reviews[2].style.opacity = 1;
+                 //     reviews[2].style.transform = "scaleX(1) translateY(0)";
+                 //     reviews[2].style.msTransform = "scaleX(1) translateY(0)";
+                 //     reviews[2].style.WebkitTransform = "scaleX(1) translateY(0)";
+                 // };
+
+                 revealFirstReview();
+                 revealSecondReview();
+                 // revealThirdReview();
+
+             };
