4d9f9984f8b86edfa0772c5e99310ce3218b0630
Stephanie Strong <sstrong@wideopenwest.com>
2018-02-02 13:54:54 -0700

    Stand Up Placeholder TV Content

--- scripts/digital.js
+++ scripts/digital.js
@@ -1,20 +1,20 @@
 var animateReviews = function() {
 
-                 var reviews = document.getElementsByClassName('review');
+      var reviews = document.getElementsByClassName('review');
 
-                 var revealFirstReview = function() {
-                     reviews[0].style.opacity = 1;
-                     reviews[0].style.transform = "scaleX(1) translateY(0)";
-                     reviews[0].style.msTransform = "scaleX(1) translateY(0)";
-                     reviews[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
+      var revealFirstReview = function() {
+          reviews[0].style.opacity = 1;
+          reviews[0].style.transform = "scaleX(1) translateY(0)";
+          reviews[0].style.msTransform = "scaleX(1) translateY(0)";
+          reviews[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+      };
 
-                 var revealSecondReview = function() {
-                     reviews[1].style.opacity = 1;
-                     reviews[1].style.transform = "scaleX(1) translateY(0)";
-                     reviews[1].style.msTransform = "scaleX(1) translateY(0)";
-                     reviews[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
+      var revealSecondReview = function() {
+          reviews[1].style.opacity = 1;
+          reviews[1].style.transform = "scaleX(1) translateY(0)";
+          reviews[1].style.msTransform = "scaleX(1) translateY(0)";
+          reviews[1].style.WebkitTransform = "scaleX(1) translateY(0)";
+        };
 
                  // var revealThirdReview = function() {
                  //     reviews[2].style.opacity = 1;
@@ -23,8 +23,12 @@ var animateReviews = function() {
                  //     reviews[2].style.WebkitTransform = "scaleX(1) translateY(0)";
                  // };
 
-                 revealFirstReview();
-                 revealSecondReview();
+        revealFirstReview();
+        revealSecondReview();
                  // revealThirdReview();
-                 alert("hello");
-             };
+};
+
+  window.onload = function() {
+    window.addEventListener('scroll', function(event) {
+       console.log(event);
+  }
