6150169700efb0d34dc5ae10b18ab9df4af76e46
Stephanie Strong <sstrong@wideopenwest.com>
2018-02-09 14:47:21 -0700

    TV Page Updated, Toggle active View on contact us

--- scripts/navStateChange.js
+++ scripts/navStateChange.js
@@ -1,11 +1,41 @@
-var navContainer = document.getElementByClassName("navBarRight");
+// get elements by ID
+var navs = document.getElementsByClassName('btn')
 
-var btns = navContainer.getElementByClassName("btn");
-
-for( vari=0; i<btns.length; i++){
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+// toggle function
+let toggle = function () {
+  // find the active elements and turn them off
+  let active = document.getElementsByClassName('active')
+  for ( let i = 0; i < active.length; i++ ) {
+    active[i].classList.remove('active')
+  }
+  // change the clicked to active
+  this.classList.add('active')
 }
+
+// add event listeners to each nav item
+for ( let i = 0; i < navs.length; i++ ) {
+  navs[i].addEventListener('click', toggle, false)
+}
+//
+// $(document).ready(function () {
+//
+//   // Set BaseURL
+//   var baseURL = 'index.html'
+//
+//   // Get current URL and replace baseURL
+//   var href = window.location.href.replace(baseURL, '');
+//
+//   // Remove trailing slash
+//   href = href.substr(-1) == '/' ? href.substr(0, href.length - 1) : href;
+//
+//   // Get last part of current URL
+//   var page = href.substr(href.lastIndexOf('/') + 1);
+//
+//   // Add trailing slash if not empty (empty means we're currently at baseURL)
+//   page = page != '' ? page + '/' : page;
+//
+//   // Select link based on href attribute and set it's closest 'li' to 'active'.
+//   // .siblings('.active').removeClass() is only needed if you have a default 'active li'.
+//   $('a[href="' + baseURL + page + '"]', '.nav li').closest('li').addClass('active').siblings('.active').removeClass();
+//
+// });
